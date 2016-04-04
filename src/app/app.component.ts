f56394e70b34a6ce413204973d45917e1d5e187e
Ludevik <ludovit.hajzer@gmail.com>
2016-04-04 21:53:33 +0200

    toh-3-master-detail

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,19 @@
 import {Component} from "angular2/core";
 import {Hero} from "./hero";
 
+let HEROES:Hero[] = [
+    {"id": 11, "name": "Mr. Nice"},
+    {"id": 12, "name": "Narco"},
+    {"id": 13, "name": "Bombasto"},
+    {"id": 14, "name": "Celeritas"},
+    {"id": 15, "name": "Magneta"},
+    {"id": 16, "name": "RubberMan"},
+    {"id": 17, "name": "Dynama"},
+    {"id": 18, "name": "Dr IQ"},
+    {"id": 19, "name": "Magma"},
+    {"id": 20, "name": "Tornado"}
+];
+
 @Component({
     selector: 'app',
     styles: [require('./app.component.scss')],
@@ -9,5 +22,10 @@ import {Hero} from "./hero";
 export class AppComponent {
 
     public title:string = 'Tour of Heroes';
-    public hero:Hero = new Hero(1, 'Spiderman');
+    public selectedHero:Hero;
+    public heroes:Hero[] = HEROES;
+
+    public onSelect(hero:Hero) {
+        this.selectedHero = hero;
+    }
 }
